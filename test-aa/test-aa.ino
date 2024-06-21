3944da7161899cf9993133659d0ebee0b3eab914
wa1tnr <wa1tnr@users.noreply.github.com>
2024-06-21 17:19:17 +0000

    adds GPIO test code
      compiles.
    
      no effort made to validate pin 14 (P1.4) as appropriate
      to the Adafruit board.
    
    	modified:   test-aa.ino
    
    On branch dvlp-aa

--- test-aa/test-aa.ino
+++ test-aa/test-aa.ino
@@ -1,11 +1,25 @@
 /* test-aa.ino */
 /* Fri 21 Jun 17:05:42 UTC 2024 */
 
+#define CS_PIN 14
+
 void waitUSB() {
     while (!USBSerial()) {
         ; // wait for serial port to connect.
           // Needed for native USB port only
     }
+}
+
+void gpio_setup() {
+
+    /*
+     * upstream has dWrite before pinMode
+     * perhaps to avoid electrical issues
+     * that may arise if not done in this sequence.
+     */
+
+    digitalWrite(CS_PIN, HIGH);
+    pinMode(CS_PIN, OUTPUT);
 }
 
 void setup() { USBSerial_println("ASCII Table ~ Character Map"); }
